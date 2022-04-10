144bb307d1e5a2a3dbf4dded60d2e0b46c148177
Селбаев Алмас <almasselbayev@gmail.com>
2022-04-10 17:14:43 +0600

    Initial Commit

---
+++ Homework 2.10 URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.10 URLSession
+//
+//  Created by Almas Selbayev on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
